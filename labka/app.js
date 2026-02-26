5d61c63c992d4bc6ef3f8938820ad6740b66f34e
kyst1kys <kooooostik1979@gmail.com>
2026-02-26 17:58:45 +0200

    bug fix

--- labka/app.js
+++ labka/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 
-// Імпортуємо наш роутер
 const studentsRouter = require('./routes/students');
 
 const app = express();
@@ -13,8 +12,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Використовуємо роутер.
-// Всі шляхи всередині studentsRouter тепер будуть починатися з /student
+
 app.use('/student', studentsRouter);
 
 app.listen(8083, () => console.log('Сервер: http://localhost:8083'));
